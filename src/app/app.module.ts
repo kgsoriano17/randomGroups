a6c46a67d5ff77428c0df72731eb979efce9d46d
Kevin Gil Rausa Soriano <ksoriano@ad.acquireap>
2018-05-11 15:20:59 +0800

    add handle error initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -23,7 +26,10 @@ import { AppRoutingModule } from './/app-routing.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dev.service.ts
+++ src/app/dev.service.ts
@@ -6,17 +6,43 @@ import { Devs } from './devs';
 import { TEAM1, TEAM2, TEAM3, TEAM4 } from './mock-devs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({ providedIn: 'root' })
 export class DevService {
+  private devsUrl = 'api/devs';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getDevs(): Observable<Devs[]> {
     this.messageService.add('DevService: fetched Developers');
-    return of(TEAM1);
+    return this.http.get<Devs[]>(this.devsUrl).pipe(
+      catchError(this.handleError('getDevs', []))
+    );
   }
   getDev(id: number): Observable<Devs> { // Devs[]
     this.messageService.add('DevService: fetched Developers');
     return of(TEAM1.find(dev => dev.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
